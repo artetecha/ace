20daa5af2bd2a4eb6b6e0f3d5bddbe44bbaf2628
Vincenzo Russo <nemo@neminis.org>
2015-10-23 23:37:25 +0100

    jQuery Settings.

--- profiles/ace/settings/settings.global.php
+++ profiles/ace/settings/settings.global.php
@@ -1,4 +1,16 @@
 <?php
 
+/**
+ * Performance.
+ */
 $conf['preprocess_css'] = 1;
 $conf['preprocess_js'] = 1;
+
+/**
+ * jQuery Update.
+ */
+$conf['jquery_update_jquery_version'] = '1.7';
+$conf['jquery_update_jquery_admin_version'] = '1.7';
+$conf['jquery_update_compression_type'] = 'min';
+
+
